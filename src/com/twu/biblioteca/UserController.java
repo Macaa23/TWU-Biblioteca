034e62934cf601bb16a2d522495d67abc8237275
Rocio Sepulveda <rocio_23s@hotmail.com>
2018-05-12 20:56:49 -0300

    User Controller Added

--- src/com/twu/biblioteca/UserController.java
+++ src/com/twu/biblioteca/UserController.java
@@ -4,8 +4,15 @@ public class UserController {
 
     private UserDao userDao = new UserDaoImpl();
 
-    public boolean checkLibraryNumber(String number) {
-        userDao.findByLibraryNumber(number);
-        return true;
+    public boolean checkLibraryNumberFormat(String libraryNumber) {
+        if(libraryNumber.matches("^\\d{3}\\-\\d{4}")) return true;
+        else return false;
+    }
+
+    public String login(String libraryNumber, String password) {
+        if(!checkLibraryNumberFormat(libraryNumber)) return "\nThe library number must follow the format: ddd-dddd\n";
+        User requiredUser = userDao.findByLibraryNumber(libraryNumber);
+        if(requiredUser == null || !requiredUser.getPassword().equals(password)) return "\nThe library number or password are incorrect.\n";
+        else return "\nLog-in Successful.\n";
     }
 }
--- test/com/twu/biblioteca/UserControllerTest.java
+++ test/com/twu/biblioteca/UserControllerTest.java
@@ -21,9 +21,44 @@ public class UserControllerTest {
     @InjectMocks
     private UserController userController;
 
+
     @Test
-    public void checkLibraryNumber_shouldReturnTrue_whenTheNumberExistsInTheRegistries(){
-        when(userDao.findByLibraryNumber(rocio.getLibraryNumber())).thenReturn(rocio);
-        assertThat(userController.checkLibraryNumber("123-1234"), is(true));
+    public void checkLibraryNumberFormat_shouldReturnTrue_whenTheNumberFollowsTheFormat(){
+        String rightNumber = "576-0039";
+        assertThat(userController.checkLibraryNumberFormat(rightNumber), is(true));
     }
+
+    @Test
+    public void checkLibraryNumberFormat_shouldReturnFalse_whenTheNumberDoesntFollowTheFormat(){
+        String wrongNumber = "123123-4";
+        assertThat(userController.checkLibraryNumberFormat(wrongNumber), is(false));
+    }
+
+    @Test
+    public void login_shouldReturnAFailureMessage_whenTheUserDoesNotExists(){
+        String falseNumber = "000-0000";
+        String falsePassword = "password";
+        when(userDao.findByLibraryNumber(falseNumber)).thenReturn(null);
+        assertThat(userController.login(falseNumber, falsePassword), is("\nThe library number or password are incorrect.\n"));
+    }
+
+    @Test
+    public void login_shouldReturnAnErrorMessage_whenTheLibraryNumberDoesntFollowTheFormat(){
+        String wrongNumber = "123123-4";
+        assertThat(userController.login(wrongNumber, rocio.getPassword()), is("\nThe library number must follow the format: ddd-dddd\n"));
+    }
+
+    @Test
+    public void login_shouldReturnAFailureMessage_whenTheUserExistsAndThePasswordDoesntMatchTheUser(){
+        String incorrectPassword = "password";
+        when(userDao.findByLibraryNumber(rocio.getLibraryNumber())).thenReturn(rocio);
+        assertThat(userController.login(rocio.getLibraryNumber(), incorrectPassword), is("\nThe library number or password are incorrect.\n"));
+    }
+
+    @Test
+    public void login_shouldReturnASuccessMessage_whenTheUserExistsAndThePasswordMatchesTheUser(){
+        when(userDao.findByLibraryNumber(rocio.getLibraryNumber())).thenReturn(rocio);
+        assertThat(userController.login(rocio.getLibraryNumber(), rocio.getPassword()), is("\nLog-in Successful.\n"));
+    }
+
 }
